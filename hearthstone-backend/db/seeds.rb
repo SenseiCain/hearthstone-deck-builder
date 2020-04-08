7b83f37ccc9ca36548a2815e777a342da0a4db3e
Christian Cain <christian22cain@yahoo.com>
2020-04-08 13:19:03 -0500

    creates hero and card instances

--- hearthstone-backend/db/seeds.rb
+++ hearthstone-backend/db/seeds.rb
@@ -8,26 +8,51 @@ cards_data = json_data["cards"][0..2]
 
 
 
-# "cardId": "HERO_09",
-# "dbfId": "813",
-# "name": "Anduin Wrynn",
+# "cardId": "CS2_041",
+# "dbfId": "149",
+# "name": "Ancestral Healing",
 # "cardSet": "Basic",
-# "type": "Hero",
+# "type": "Spell",
 # "faction": "Neutral",
 # "rarity": "Free",
-# "health": 30,
+# "cost": 0,
+# "text": "Restore a minion\\nto full Health and\\ngive it <b>Taunt</b>.",
+# "flavor": "I personally prefer some non-ancestral right-the-heck-now healing, but maybe that is just me.",
+# "artist": "Dan Scott",
 # "collectible": true,
-# "playerClass": "Priest",
-# "img": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/HERO_09.png",
-# "imgGold": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/HERO_09_premium.gif",
-# "locale": "enUS"
+# "playerClass": "Shaman",
+# "howToGet": "Unlocked at Level 1.",
+# "howToGetGold": "Unlocked at Level 15.",
+# "img": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/CS2_041.png",
+# "imgGold": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_041_premium.gif",
+# "locale": "enUS",
+# "mechanics": [
+#     {
+#         "name": "Taunt"
+#     }
+# ]
 
 heros_data.each do |obj|
     Hero.create(
         card_id: obj["cardId"],
-        dbf_id: obj["bdfId"],
+        dbf_id: obj["dbfId"],
         name: obj["name"],
         player_class: obj["playerClass"],
         img: "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/#{obj["cardId"]}.png"
+    )
+end
+
+cards_data.each do |obj|
+    Card.create(
+        card_id: obj["cardId"],
+        dbf_id: obj["dbfId"],
+        name: obj["name"],
+        card_set: obj["cardSet"],
+        card_type: obj["type"],
+        rarity: obj["rarity"],
+        cost: obj["cost"],
+        player_class: obj["playerClass"],
+        img: "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/#{obj["cardId"]}.png",
+        mechanics: obj["mechanics"]
     )
 end
