5899cd2552167c3b4595d8d08360e60a890af00d
Christian Cain <christian22cain@yahoo.com>
2020-04-08 12:58:55 -0500

    removed cached data

--- hearthstone-backend/db/seeds.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-
-json_file = File.read('./hearthstone-backend/db/cards.json')
-json_data = JSON.parse(json_file)
-
-heros_test = json_data["heros"][0..2]
-cards_test = json_data["cards"][0..2]
