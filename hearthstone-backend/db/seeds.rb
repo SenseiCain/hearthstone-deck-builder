053db6ddbf246cdae8ed33a5130e56e0e222f6a2
Christian Cain <christian22cain@yahoo.com>
2020-04-08 12:40:27 -0500

    added static json data

--- hearthstone-backend/db/seeds.rb
+++ hearthstone-backend/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+json_file = File.read('./hearthstone-backend/db/cards.json')
+json_data = JSON.parse(json_file)
+
+heros_test = json_data["heros"][0..2]
+cards_test = json_data["cards"][0..2]
