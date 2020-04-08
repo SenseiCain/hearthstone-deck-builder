a1bb8acde9bd019fcbecbf3720079cee1382796a
Christian Cain <christian22cain@yahoo.com>
2020-04-08 13:20:23 -0500

    processes all data

--- hearthstone-backend/db/seeds.rb
+++ hearthstone-backend/db/seeds.rb
@@ -3,34 +3,8 @@ require 'json'
 json_file = File.read('./db/cards.json')
 json_data = JSON.parse(json_file)
 
-heros_data = json_data["heros"][0..2]
-cards_data = json_data["cards"][0..2]
-
-
-
-# "cardId": "CS2_041",
-# "dbfId": "149",
-# "name": "Ancestral Healing",
-# "cardSet": "Basic",
-# "type": "Spell",
-# "faction": "Neutral",
-# "rarity": "Free",
-# "cost": 0,
-# "text": "Restore a minion\\nto full Health and\\ngive it <b>Taunt</b>.",
-# "flavor": "I personally prefer some non-ancestral right-the-heck-now healing, but maybe that is just me.",
-# "artist": "Dan Scott",
-# "collectible": true,
-# "playerClass": "Shaman",
-# "howToGet": "Unlocked at Level 1.",
-# "howToGetGold": "Unlocked at Level 15.",
-# "img": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/CS2_041.png",
-# "imgGold": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_041_premium.gif",
-# "locale": "enUS",
-# "mechanics": [
-#     {
-#         "name": "Taunt"
-#     }
-# ]
+heros_data = json_data["heros"]
+cards_data = json_data["cards"]
 
 heros_data.each do |obj|
     Hero.create(
