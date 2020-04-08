f73585a7a2ee7b401cf64f598c7429dc11c48d2b
Christian Cain <christian22cain@yahoo.com>
2020-04-08 13:09:26 -0500

    generate schema

---
+++ hearthstone-backend/db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_08_174854) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string "card_id"
+    t.string "dbf_id"
+    t.string "name"
+    t.string "card_set"
+    t.string "type"
+    t.string "rarity"
+    t.string "cost"
+    t.string "player_class"
+    t.string "img"
+    t.string "mechanics"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "heros", force: :cascade do |t|
+    t.string "card_id"
+    t.string "dbf_id"
+    t.string "name"
+    t.string "player_class"
+    t.string "img"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- hearthstone-backend/db/seeds.rb
+++ hearthstone-backend/db/seeds.rb
@@ -1,7 +1,33 @@
 require 'json'
 
-json_file = File.read('./hearthstone-backend/db/cards.json')
+json_file = File.read('./db/cards.json')
 json_data = JSON.parse(json_file)
 
-heros_test = json_data["heros"][0..2]
-cards_test = json_data["cards"][0..2]
+heros_data = json_data["heros"][0..2]
+cards_data = json_data["cards"][0..2]
+
+
+
+# "cardId": "HERO_09",
+# "dbfId": "813",
+# "name": "Anduin Wrynn",
+# "cardSet": "Basic",
+# "type": "Hero",
+# "faction": "Neutral",
+# "rarity": "Free",
+# "health": 30,
+# "collectible": true,
+# "playerClass": "Priest",
+# "img": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/HERO_09.png",
+# "imgGold": "http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/HERO_09_premium.gif",
+# "locale": "enUS"
+
+heros_data.each do |obj|
+    Hero.create(
+        card_id: obj["cardId"],
+        dbf_id: obj["bdfId"],
+        name: obj["name"],
+        player_class: obj["playerClass"],
+        img: "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/#{obj["cardId"]}.png"
+    )
+end
