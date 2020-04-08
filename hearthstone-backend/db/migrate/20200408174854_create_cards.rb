0f746134bbe8070333d2a3e8aa91e5aa8fe870ce
Christian Cain <christian22cain@yahoo.com>
2020-04-08 12:56:27 -0500

    fixed cache error

---
+++ hearthstone-backend/db/migrate/20200408174854_create_cards.rb
@@ -0,0 +1,18 @@
+class CreateCards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cards do |t|
+      t.string :card_id
+      t.string :dbf_id
+      t.string :name
+      t.string :card_set
+      t.string :type
+      t.string :rarity
+      t.string :cost
+      t.string :player_class
+      t.string :img
+      t.string :mechanics
+
+      t.timestamps
+    end
+  end
+end
