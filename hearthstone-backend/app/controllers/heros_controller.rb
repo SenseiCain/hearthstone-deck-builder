6ec6b078678fe76f922387204f3eaed895fa8daa
Christian Cain <christian22cain@yahoo.com>
2020-04-08 13:34:30 -0500

    added index heros

--- hearthstone-backend/app/controllers/heros_controller.rb
+++ hearthstone-backend/app/controllers/heros_controller.rb
@@ -1,6 +1,16 @@
 class HerosController < ApplicationController
+    def index
+        heros = Hero.all
+        render json: heros
+    end
+
     def show
-        hero = Hero.find_by(params[:id])
-        render json: hero
+        hero = Hero.find_by(id: params[:id])
+
+        if hero
+            render json: hero
+        else
+            render json: { message: "Hero not found" }
+        end
     end
 end
