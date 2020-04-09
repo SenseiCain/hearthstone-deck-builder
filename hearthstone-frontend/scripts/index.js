41f320486827c6ff0a1d4d14c6712a092e1f7727
Christian Cain <christian22cain@yahoo.com>
2020-04-09 11:02:53 -0500

    abstracted data retrieval & render fns

--- hearthstone-frontend/scripts/index.js
+++ hearthstone-frontend/scripts/index.js
@@ -1,12 +1,16 @@
 const BASE_URL = 'http://localhost:3000/cards'
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    getCards('Hunter')
+    renderCards('Hunter')
 });
+
+async function renderCards(className) {
+    const card_data = await getCards(className)
+    console.log(card_data)
+}
 
 async function getCards(className) {
     const resp = await fetch(`${BASE_URL}?class=${className}`)
     const json = await resp.json()
-
-    console.log(json)
+    return json
 }
