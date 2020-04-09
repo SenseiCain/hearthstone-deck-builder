b64d45ca338469e3d86bb477fabf4c0f7955fd36
Christian Cain <christian22cain@yahoo.com>
2020-04-09 11:42:04 -0500

    added buttons for heros that fetch associated cards

--- hearthstone-frontend/src/index.js
+++ hearthstone-frontend/src/index.js
@@ -1,16 +1,39 @@
-const BASE_URL = 'http://localhost:3000/cards'
+const BASE_URL = 'http://localhost:3000'
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    renderCards('Hunter')
+    renderHeros()
 });
 
+
+// -- HEROS --
+async function getHeros() {
+    const resp = await fetch(`${BASE_URL}/heros`)
+    const json = await resp.json()
+    return json
+}
+
+async function renderHeros() {
+    const heroData = await getHeros()
+    const heroContainerEl = document.querySelector('#heros-section')
+
+    heroData.forEach(el => {
+        const btn = document.createElement('button')
+        btn.innerText = el.player_class
+        btn.id = `btn-${el.player_class.replace(/\s+/g, '-').toLowerCase()}`;
+        btn.addEventListener('click', e => renderCards(el.player_class))
+
+        heroContainerEl.appendChild(btn)
+    })
+}
+
+// -- CARDS --
 async function renderCards(className) {
-    const card_data = await getCards(className)
-    console.log(card_data)
+    const cardData = await getCards(className)
+    console.log(cardData)
 }
 
 async function getCards(className) {
-    const resp = await fetch(`${BASE_URL}?class=${className}`)
+    const resp = await fetch(`${BASE_URL}/cards?class=${className}`)
     const json = await resp.json()
     return json
 }
