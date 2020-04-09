ef152b344b1432599bbc44efe8fad74954c60450
Christian Cain <christian22cain@yahoo.com>
2020-04-09 10:45:54 -0500

    added query feature by class name

--- hearthstone-backend/app/controllers/cards_controller.rb
+++ hearthstone-backend/app/controllers/cards_controller.rb
@@ -1,6 +1,11 @@
 class CardsController < ApplicationController
     def index
-        cards = Card.all[0..3]
-        render json: cards
+        if params[:class]
+            cards = Card.find_by_class(params[:class])
+        else
+            cards = Card.all
+        end
+
+        render json: cards[0..3]
     end
 end
--- hearthstone-backend/app/models/card.rb
+++ hearthstone-backend/app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ApplicationRecord
+    def self.find_by_class(className)
+        self.where(player_class: className)
+    end
 end
