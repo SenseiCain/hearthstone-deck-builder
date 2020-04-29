2da8978c7e3e0d885316db0f7d154abcc9abbecd
Christian Cain <christian22cain@yahoo.com>
2020-04-29 14:51:45 -0500

    added basic validations for deck

--- hearthstone-backend/app/controllers/decks_controller.rb
+++ hearthstone-backend/app/controllers/decks_controller.rb
@@ -1,13 +1,11 @@
 class DecksController < ApplicationController
     def create
         req_data = JSON.parse(request.body.read)
+        cards = req_data.map{|obj| Card.find_by(card_id: obj["card_id"])}
+        deck = Deck.new
+        deck.cards << cards
         
-        if req_data.count == 30
-            cards = req_data.map{|obj| Card.find_by(card_id: obj["card_id"])}
-            deck = Deck.new
-            deck.cards << cards
-            deck.save
-
+        if deck.save
             render json: {message: 'success'}
         else
             render json: { message: 'failure' }
--- hearthstone-backend/app/models/deck.rb
+++ hearthstone-backend/app/models/deck.rb
@@ -1,3 +1,16 @@
 class Deck < ApplicationRecord
     has_and_belongs_to_many :cards
+
+    validate :validate_deck
+
+    def validate_deck
+        # Need to add validations for:
+            # Only two copies of regular cards
+            # Only one copy of legendary cards
+            # Only one deck with same name & card combo
+            
+        if self.cards.length != 30
+            errors.add(:length, "Must have 30 cards")
+        end
+    end
 end
